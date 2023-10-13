eab78740c487addb4b04112f716543024e91e719
josewaihiga <josewaihiga@gmail.com>
2023-10-13 22:44:16 +1100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from "./logo.svg";
+import "./App.css";
+import CardList from "./components/card-list/card-list.component";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+
+    console.log("constructor");
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+
+    console.log("componentDidMount");
+  }
+
+  onSearchChange = (event)=>{
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  }
+
+  render() {
+    console.log("render");
+
+    const {monsters, searchField} = this.state
+    const {onSearchChange} = this
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    return (
+      <div className="App">
+
+      <CardList/>
+
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={onSearchChange}
+        />
+        {filteredMonsters.map((monster, i) => {
+          return <h1 key={monster.id + i}>{monster.name}</h1>;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class CardList extends Component {
+  render() {
+    return <div>Hello I'm the card list component</div>;
+  }
+}
+
+export default CardList
